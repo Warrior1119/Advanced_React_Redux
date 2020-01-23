ff85f3f755d5fcebf854877088f2095e5c0af2da
WebDev1119 <warriorjhs@outlook.com>
2020-01-23 04:28:24 -1000

    add commentBox

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default () => {
-  return <div>Comment Box</div>;
-};
+export default class CommentBox extends Component {
+  state = { comment: "" };
+
+  constructor(props) {
+    super(props);
+  }
+
+  handleChange = e => {
+    this.setState({ comment: e.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    // TODO - Call an action creator
+    // And save the comment
+    this.setState({ comment: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}

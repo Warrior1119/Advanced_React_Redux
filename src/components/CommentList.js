4f2942651d9d0a6c70cf9f52a17f3cfe5bda180e
WebDev1119 <warriorjhs@outlook.com>
2020-01-23 03:23:32 -1000

    update absolute path

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,9 +1,5 @@
-import React from 'react';
+import React from "react";
 
 export default () => {
-    return (
-        <div>
-            Command List
-        </div>
-    );
+  return <div>Command List</div>;
 };
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
 
-it('shows a comment box', ()=> {
-    const div = document.createElement('div');
+it("shows a comment box", () => {
+  const wrapped = shallow(<App />);
 
-    ReactDOM.render(<App />, div);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-    // Looks inside the div
-    // and checks to see if the CommentBox is in there
-    // console.log(div.innerHTML);
-    expect(div.innerHTML).toContain("Comment Box");
+it("shows a comment list", () => {
+  const wrapped = shallow(<App />);
 
-    ReactDOM.unmountComponentAtNode(div);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });

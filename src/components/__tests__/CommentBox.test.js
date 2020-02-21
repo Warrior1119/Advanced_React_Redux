613adc90f19e1d39b7a720cbbffccf8e3ed26d6b
WebDev1119 <warriorjhs@outlook.com>
2020-02-21 21:13:22 +0800

    add commentbox fulldom test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -2,12 +2,28 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
+let wrapped;
+
+beforeEach(() => {
+	wrapped = mount(<CommentBox />);
+
+});
+
+afterEach(() => {
+	wrapped.unmount();
+})
 
 it('has a text area and a button', () => {
-    const wrapped = mount(<CommentBox />);
-    console.log(wrapped.find("textarea").length);
-    console.log(wrapped.find("button").length);
-
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
-})
+});
+
+it('has a text area that user can type in', () => {
+	wrapped.find('textarea').simulate('change', {
+		target: {value: 'new comment'}
+	});
+
+	wrapped.update();
+
+	expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
